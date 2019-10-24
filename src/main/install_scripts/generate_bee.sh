a5542795269ffc52fbdd91065802ee02f7c4e351
dalaocu <njumjy06@126.com>
2019-10-24 16:21:44 +0800

    script

--- src/main/install_scripts/generate_bee.sh
+++ src/main/install_scripts/generate_bee.sh
@@ -191,13 +191,13 @@ rm -rf $BM
 git clone https://github.com/WeBankFinTech/$BM.git
 cd $BM
 
-git checkout master
+git checkout dev_type_handle_2019.10
 cd ..
 
 rm -rf $BB
 git clone https://github.com/WeBankFinTech/$BB.git
 cd $BB
-git checkout master
+git checkout dev_type_handle_2019.10
 cd ..
 
 # init config
