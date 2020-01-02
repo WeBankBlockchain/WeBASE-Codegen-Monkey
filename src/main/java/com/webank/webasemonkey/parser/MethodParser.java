4cdf8b9912b6049b2202720801cc85fe8903ba1d
dalaocu <njumjy06@126.com>
2020-01-02 17:36:18 +0800

    refactor MethodParser

--- src/main/java/com/webank/webasemonkey/parser/MethodParser.java
+++ src/main/java/com/webank/webasemonkey/parser/MethodParser.java
@@ -20,7 +20,9 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.assertj.core.util.Arrays;
 import org.fisco.bcos.web3j.abi.TypeReference;
 import org.fisco.bcos.web3j.protocol.ObjectMapperFactory;
 import org.fisco.bcos.web3j.protocol.core.methods.response.AbiDefinition;
@@ -59,8 +61,9 @@ public class MethodParser implements ContractJavaParserInterface<MethodMetaInfo>
 
     public List<MethodMetaInfo> parseToInfoList(Class<?> clazz) {
         AbiDefinition[] abiDefinitions = getContractAbiList(clazz);
-        if (abiDefinitions == null || abiDefinitions.length == 0)
+        if (Arrays.isNullOrEmpty(abiDefinitions)) {
             return null;
+        }
         List<MethodMetaInfo> lists = Lists.newArrayList();
         for (AbiDefinition abiDefinition : abiDefinitions) {
             String abiType = abiDefinition.getType();
@@ -68,23 +71,20 @@ public class MethodParser implements ContractJavaParserInterface<MethodMetaInfo>
                 continue;
             }
             List<NamedType> inputs = abiDefinition.getInputs();
-            if (inputs == null || inputs.isEmpty()) {
+            if (CollectionUtils.isEmpty(inputs)) {
                 continue;
             }
             // solidity new feature: selector(which returns a method id) may have no name.
             if (StringUtils.isEmpty(inputs.get(0).getName())) {
                 continue;
             }
-
             MethodMetaInfo method = new MethodMetaInfo();
             method.setContractName(clazz.getSimpleName());
-            String Name = abiDefinition.getName();
-            log.debug("method name : {}", Name);
-
+            log.debug("method name : {}", abiDefinition.getName());
             if (abiType.equals(AbiTypeConstants.ABI_CONSTRUCTOR_TYPE)) {
                 method.setName(clazz.getSimpleName());
             } else {
-                method.setName(Name);
+                method.setName(abiDefinition.getName());
             }
             String generatedFlag = PropertiesUtils.getGlobalProperty(ParserConstants.MONITOR, method.getContractName(),
                     method.getName(), "generated", "on");
@@ -93,35 +93,36 @@ public class MethodParser implements ContractJavaParserInterface<MethodMetaInfo>
             }
             int shardingNO = Integer.parseInt(PropertiesUtils.getGlobalProperty(ParserConstants.SYSTEM,
                     method.getContractName(), method.getName(), ParserConstants.SHARDINGNO, "1"));
-            method.setShardingNO(shardingNO);
-
-            ArrayList<FieldVO> fieldList = Lists.newArrayList();
-
-            for (NamedType namedType : inputs) {
-                FieldVO vo = new FieldVO();
-                String k = namedType.getName();
-                // 增加is前缀变量的特殊处理
-                if (StringUtils.startsWith(k, "is") && k.length() > 2 && Character.isUpperCase(k.charAt(2))) {
-                    k = StringUtils.uncapitalize(StringUtils.substring(k, 2));
-                }
-                String type = namedType.getType().split(" ")[0];
-                String v = solidityType2SolidityReferenceType(type);
-                String length = PropertiesUtils.getGlobalProperty(ParserConstants.LENGTH, method.getContractName(),
-                        method.getName(), k, "0");
-                String sqlName = systemEnvironmentConfig.getNamePrefix() + StringStyleUtils.upper2underline(k)
-                        + systemEnvironmentConfig.getNamePostfix();
-                vo.setSolidityName(k).setSqlName(sqlName).setJavaName(k).setSqlType(Web3jTypeEnum.parse(v).getSqlType())
-                        .setSolidityType(v).setJavaType(Web3jTypeEnum.parse(v).getJavaType())
-                        .setTypeMethod(Web3jTypeEnum.parse(v).getTypeMethod()).setJavaCapName(StringUtils.capitalize(k))
-                        .setLength(Integer.parseInt(length));
-                log.debug("java name {}, java type {}, solidity type {}, type method {}", vo.getJavaName(),
-                        vo.getJavaType(), vo.getSolidityType(), vo.getTypeMethod());
-                fieldList.add(vo);
-            }
-            method.setList(fieldList);
+            method.setShardingNO(shardingNO).setList(getFieldList(method, inputs));
             lists.add(method);
         }
         return lists;
+    }
+
+    public ArrayList<FieldVO> getFieldList(MethodMetaInfo method, List<NamedType> inputs) {
+        ArrayList<FieldVO> fieldList = Lists.newArrayList();
+        for (NamedType namedType : inputs) {
+            FieldVO vo = new FieldVO();
+            String k = namedType.getName();
+            // 增加is前缀变量的特殊处理
+            if (StringUtils.startsWith(k, "is") && k.length() > 2 && Character.isUpperCase(k.charAt(2))) {
+                k = StringUtils.uncapitalize(StringUtils.substring(k, 2));
+            }
+            String type = namedType.getType().split(" ")[0];
+            String v = solidityType2SolidityReferenceType(type);
+            String length = PropertiesUtils.getGlobalProperty(ParserConstants.LENGTH, method.getContractName(),
+                    method.getName(), k, "0");
+            String sqlName = systemEnvironmentConfig.getNamePrefix() + StringStyleUtils.upper2underline(k)
+                    + systemEnvironmentConfig.getNamePostfix();
+            vo.setSolidityName(k).setSqlName(sqlName).setJavaName(k).setSqlType(Web3jTypeEnum.parse(v).getSqlType())
+                    .setSolidityType(v).setJavaType(Web3jTypeEnum.parse(v).getJavaType())
+                    .setTypeMethod(Web3jTypeEnum.parse(v).getTypeMethod()).setJavaCapName(StringUtils.capitalize(k))
+                    .setLength(Integer.parseInt(length));
+            log.debug("java name {}, java type {}, solidity type {}, type method {}", vo.getJavaName(),
+                    vo.getJavaType(), vo.getSolidityType(), vo.getTypeMethod());
+            fieldList.add(vo);
+        }
+        return fieldList;
     }
 
     /**
